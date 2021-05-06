e1d546a6874183621cc3ddadab803a716d7d8a1e
Niki <nikimortazavi21@gmail.com>
2021-05-06 09:36:20 +0200

    Background color change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from "react";
 import "./Styles.css";
+import Header from "./components/Header";
+import FetchedMovies from "./components/FetchedMovies";
+import { Helmet } from "react-helmet";
+import Popular from "./components/Popular";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Helmet>
+        <style>{"body { background-color: #1b1c22; }"}</style>
+      </Helmet>
+      <Header />
+      <Popular />
+      <FetchedMovies />
+    </>
+  );
 }
 
 export default App;
